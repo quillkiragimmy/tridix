3322f39435c244e4b16ae940a6beca08170d4cd1
Patricius <quillkiragimmy@gmail.com>
2014-12-23 01:09:49 +0800

    Prepare to merge into master.

--- tridix.sh
+++ tridix.sh
@@ -8,6 +8,29 @@ txtred='\e[0;31m' # Red
 txtgrn='\e[0;32m' # Green
 txtylw='\e[0;33m' # Yellow
 txtblu='\e[0;34m' # Blueused by j/ldic.
+txtblu='\e[0;34m' # Blue
+txtpur='\e[0;35m' # Purple
+txtcyn='\e[0;36m' # Cyan
+txtwht='\e[0;37m' # White
+txtdft='\e[0;39m' # Default
+
+bldblk='\e[1;30m' # Black - Bold
+bldred='\e[1;31m' # Red
+bldgrn='\e[1;32m' # Green
+bldylw='\e[1;33m' # Yellow
+bldblu='\e[1;34m' # Blue
+bldpur='\e[1;35m' # Purple
+bldcyn='\e[1;36m' # Cyan
+bldwht='\e[1;37m' # White
+
+diclist=~/tridixlist.list
+mode='En'     #en,ja,la.
+termgeom=( "$(tput lines)" "$(tput cols)" )
+src=''
+tmp=''
+last=''
+meta='' # related stuff.
+bars=''       # used by j/ldic.
 
 trap "rm -f $tmp $src && exit 0" SIGHUP SIGINT SIGTERM 
 
