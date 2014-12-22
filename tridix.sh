6011c11805d4c30bbc11f5e87a43868d99d6cd2f
Patricius <quillkiragimmy@gmail.com>
2014-12-23 01:15:36 +0800

    wowow

--- tridix.sh
+++ tridix.sh
@@ -23,6 +23,7 @@ bldpur='\e[1;35m' # Purple
 bldcyn='\e[1;36m' # Cyan
 bldwht='\e[1;37m' # White
 
+wowowo
 diclist=~/tridixlist.list
 mode='En'	#en,ja,la.
 termgeom=( "$(tput lines)" "$(tput cols)" )
