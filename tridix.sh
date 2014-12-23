ace0b62566553a1bae6a0670edf2d5a72f9cd0e3
Patricius <quillkiragimmy@gmail.com>
2014-12-23 11:22:00 +0800

    Use uppercase for all globals, replace [ -z ] with [[ ]]. For the sake of style and simplicity.

--- tridix.sh
+++ tridix.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/bash
 
-	# text colors.
+##############################
+# text colors.
+##############################
 txtrst='\e[0m'    # Text Reset
 
 txtblk='\e[0;30m' # Black - Regular
@@ -23,17 +25,23 @@ bldpur='\e[1;35m' # Purple
 bldcyn='\e[1;36m' # Cyan
 bldwht='\e[1;37m' # White
 
-diclist=~/tridixlist.list
-mode='En'	#en,ja,la.
-termgeom=( "$(tput lines)" "$(tput cols)" )
-src=''
-tmp=''
-last=''
-meta='' # related stuff.
-bars=''       # used by j/ldic.
+##############################
+# gloable vars.
+##############################
+DICLIST=~/tridixlist.list
+MODE='En'	#en,ja,la.
+TERMGEOM=( "$(tput lines)" "$(tput cols)" )
+SOURCE=''
+TEMP=''
+LAST=''
+META='' # related stuff.
+BARS=''       # used by j/ldic.
 
-trap "rm -f $tmp $src && exit 0" SIGHUP SIGINT SIGTERM 
+trap "rm -f $TEMP $SOURCE && exit 0" SIGHUP SIGINT SIGTERM 
 
+##############################
+# dictionary functions.
+##############################
 engcolorize(){
 	while read line; do
 		echo -e "$(echo "$line"\
@@ -46,44 +54,44 @@ engcolorize(){
 }
 
 endic(){
-	curl -sL http://dictionary.reference.com/browse/$(echo "$@"| tr ' ' '+')?s=t > $src
+	curl -sL http://dictionary.reference.com/browse/$(echo "$@"| tr ' ' '+')?s=t > $SOURCE
 
-	if [ ! -z "$(fgrep 'Did you mean' $src)" ]; then
-		xmllint --html --htmlout --xpath '//section[@class="more-suggestions"]' $src 2>/dev/null| w3m -T text/html| cat
+	if [[ $(fgrep 'Did you mean' $SOURCE) ]]; then
+		xmllint --html --htmlout --xpath '//section[@class="more-suggestions"]' $SOURCE 2>/dev/null| w3m -T text/html| cat
 	else
 			# pronounce.
-		pron="$(xmllint --html --htmlout --xpath '//*[@id="source-luna"]/div[1]/section/header/div[2]/div/span/span[2]' $src 2>/dev/null\
+		pron="$(xmllint --html --htmlout --xpath '//*[@id="source-luna"]/div[1]/section/header/div[2]/div/span/span[2]' $SOURCE 2>/dev/null\
 			| perl -pe 's/<.*?>//g')"
-		echo $pron >> $tmp
+		echo $pron >> $TEMP
 
 			# relatives.
-		rel="$(cat $src| xmllint --html --htmlout --xpath '//*[@class="tail-box tail-type-relf"]' - 2>/dev/null\
+		rel="$(cat $SOURCE| xmllint --html --htmlout --xpath '//*[@class="tail-box tail-type-relf"]' - 2>/dev/null\
 			| perl -pe 's/<.*?>//g'\
 			| grep -v '^$'\
 			| sed 's/Related forms Expand/[Related]/g'\
 			| sed 's/Derived Forms/[Derived]/g')"
 
 			# meaning.
-		xmllint --html --htmlout --xpath '//section[@class="def-pbk"]' $src 2>/dev/null\
+		xmllint --html --htmlout --xpath '//section[@class="def-pbk"]' $SOURCE 2>/dev/null\
 			| perl -pe 's/<.*?>//g'\
 			| grep -v '^$'\
-			| sed ':a;N;$!ba;s/\([0-9]\{1,2\}\.\)\n/\1) /g' >> $tmp
+			| sed ':a;N;$!ba;s/\([0-9]\{1,2\}\.\)\n/\1) /g' >> $TEMP
 
-		echo "$rel" >> $tmp
+		echo "$rel" >> $TEMP
 
-		cat $tmp| fold -w${termgeom[1]} -s | engcolorize| more -df # more fold conflicts with escaping chars (real/logical length).
+		cat $TEMP| fold -w${TERMGEOM[1]} -s | engcolorize| more -df # more fold conflicts with escaping chars (real/logical length).
 
 			# quote.
 			# incline examples.
-		quote="$(cat $tmp\
+		quote="$(cat $TEMP\
 			| grep '\.).*:'\
 			| cut -d':' -f2\
 			| head -n3\
 			| grep "$@"\
 			| sed 's/$/<br>/'\
 			| tr -d '\n')" 
-		if [ -z "$quote" ]; then # search quote from web.
-			quote="$(curl -s "$(cat $src | xmllint --html --htmlout --xpath '//*[@id="quotes-box"]/div/div/div[1]/a' - 2>/dev/null | cut -d'"' -f2) "\
+		if [[ ! $quote ]]; then # search quote from web.
+			quote="$(curl -s "$(cat $SOURCE | xmllint --html --htmlout --xpath '//*[@id="quotes-box"]/div/div/div[1]/a' - 2>/dev/null | cut -d'"' -f2) "\
 				| xmllint --html --xpath '//*[@id="o111"]' - 2>/dev/null\
 				| sed 's/<br>/. /g'| perl -pe 's/<.*?>//g'\
 				| tr '\n' ' ')"
@@ -102,60 +110,60 @@ endic(){
 			quote="$(echo "$quote"| tr -d '\n')"
 		fi
 
-		meta="$pron<br>\"$quote\"<br>$(echo "$rel"| sed 's/$/<br>/'| tr -d '\n') "
+		META="$pron<br>\"$quote\"<br>$(echo "$rel"| sed 's/$/<br>/'| tr -d '\n') "
 
 	fi
 }
 
 jadic(){
-	curl http://www.weblio.jp/content/"$@" -s > $src
-	if [ "$(cat $src| fgrep '見出し語は見つかりません')" == '' ]; then	# for error fetching.
-		xmllint --html --htmlout  --format --xpath '//div[@class="kijiWrp"]' $src 2>/dev/null\
+	curl http://www.weblio.jp/content/"$@" -s > $SOURCE
+	if [ "$(cat $SOURCE| fgrep '見出し語は見つかりません')" == '' ]; then	# for error fetching.
+		xmllint --html --htmlout  --format --xpath '//div[@class="kijiWrp"]' $SOURCE 2>/dev/null\
 			| sed 's/<!--開始/━━━━━━━━━━━━━\n@@/'\
 			| perl -pe 's/<.*?>//g'\
 			| sed 's/  /\n/g'\
 			| grep -v '@@'\
-			| grep -v '^$' > $tmp
+			| grep -v '^$' > $TEMP
 
-		bars=( $(fgrep -n "━━━━━━━━━━━━━" $tmp| cut -d':' -f1) '$' )
-		if [ "${bars[0]}" == '$' ]; then
-			bars=( '1' $bars )
+		BARS=( $(fgrep -n "━━━━━━━━━━━━━" $TEMP| cut -d':' -f1) '$' )
+		if [ "${BARS[0]}" == '$' ]; then
+			BARS=( '1' $BARS )
 		fi
-		sed -i -n "1,${bars[10]} p" $tmp 2>/dev/null
+		sed -i -n "1,${BARS[10]} p" $TEMP 2>/dev/null
 
-		cat $tmp| more -d
+		cat $TEMP| more -d
 
 	fi
 
 }
 
 ladic(){
-	echo "$@"| words| tr -d '\r' > $src
-	orig=$(cat $src| grep -n '=>$'| cut -d':' -f1)
-	end=$(cat $src| grep -n '=>Raised'| cut -d':' -f1)
-	cat $src| sed -n "$((orig+1)), $((end-1)) p"\
+	echo "$@"| words| tr -d '\r' > $SOURCE
+	orig=$(cat $SOURCE| grep -n '=>$'| cut -d':' -f1)
+	end=$(cat $SOURCE| grep -n '=>Raised'| cut -d':' -f1)
+	cat $SOURCE| sed -n "$((orig+1)), $((end-1)) p"\
 		| grep -v '^$'\
 		| tr '\r' ' '\
 		| fgrep -v 'RETURN/ENTER'\
 		| fgrep -v 'Unexpected exception'\
-		| sed 's/\(;.*\)$/\1\n━━━━━━━━━━━━━/' >> $tmp
+		| sed 's/\(;.*\)$/\1\n━━━━━━━━━━━━━/' >> $TEMP
 
-	bars=( '1' $(fgrep -n "━━━━━━━━━━━━━" $tmp| cut -d':' -f1) '$' )
+	BARS=( '1' $(fgrep -n "━━━━━━━━━━━━━" $TEMP| cut -d':' -f1) '$' )
 
-	cat $tmp| more -d
+	cat $TEMP| more -d
 
-	rm -f "$src"
+	rm -f "$SOURCE"
 }
 
 header(){
 	# another word or add previous to history.
-	echo -e "$bldred$mode)$bldgrn <word>|<index>|<command>|help; $txtrst($(wc -l $diclist | cut -d' ' -f1) listed. >>"
+	echo -e "$bldred$MODE)$bldgrn <word>|<index>|<command>|help; $txtrst($(wc -l $DICLIST | cut -d' ' -f1) listed. >>"
 }
 
 manpg(){
 	echo -e "$txtylw\tYou can enter :$txtrst"
 	echo "1. Any phrase like 'dictionary' '辞書'."
-	echo "2. 'e' / 'j' / 'l' to switch to Eng / Jap / Lat mode."
+	echo "2. 'e' / 'j' / 'l' to switch to Eng / Jap / Lat mode"
 	echo "3. '' / '1' ... '9' to add N'th word into list."
 	echo "4. Commands below :"
 	echo -e "\tls: show list entries"
@@ -164,33 +172,37 @@ manpg(){
 	echo -e "\thelp: this help"
 }
 
+##############################
+# main program.
+##############################
+
 clear
-touch "$diclist"
+touch "$DICLIST"
 header
 while read -e word; do
-	termgeom=( "$(tput lines)" "$(tput cols)" )
+	TERMGEOM=( "$(tput lines)" "$(tput cols)" )
 
 	case "$word" in
 		''|1|2|3|4|5|6|7|8|9)
 			if [ "$word" == '' ]; then word='1'; fi # enlist first word.
 
-			if [ -e "$tmp" ]; then
+			if [ -e "$TEMP" ]; then
 
-				if [ $mode == 'En' ]; then
-					echo -e "engmisc\tBasic\t1\t$last<br>$meta\t$(cat $tmp\
+				if [ $MODE == 'En' ]; then
+					echo -e "engmisc\tBasic\t1\t$LAST<br>$META\t$(cat $TEMP\
 						| tr '\n' ' '\
-						| sed 's/\( [0-9]\{1,2\}\.\)/ <br>\1/g')" >> $diclist
+						| sed 's/\( [0-9]\{1,2\}\.\)/ <br>\1/g')" >> $DICLIST
 
-				elif [ $mode == 'Ja' ]; then
-					slot=$(cat $tmp\
-						| sed -n "${bars[word-1]}, ${bars[word]} p"\
+				elif [ $MODE == 'Ja' ]; then
+					slot=$(cat $TEMP\
+						| sed -n "${BARS[word-1]}, ${BARS[word]} p"\
 						| egrep -v '^[ ]*$'\
 						| grep -v '━'\
 						| tr '\n' ' '\
 						| tr -d ' ')
 
 						# quote.
-					meta="$(echo $slot\
+					META="$(echo $slot\
 						| tr -d '\n'\
 						| sed 's/「/\n「/g' | sed 's/」.*/」/'\
 						| grep '」' | head -n5\
@@ -199,60 +211,60 @@ while read -e word; do
 
 						# find kanji(if any).
 					kanji=''
-					if [ ! -z "$(echo $slot| sed 's/\(.\{50,50\}\).*/\1/'| fgrep '【')" ]; then
+					if [[ $(echo $slot| sed 's/\(.\{50,50\}\).*/\1/'| fgrep '【') ]]; then
 						kanji="$(echo $slot| sed 's/\(.\{50,50\}\).*/\1/'| perl -pe 's/.*?(【.*?】).*/\1/')"
 					else
-						kanji="$last"
+						kanji="$LAST"
 					fi
 						# make kanji be the frontside if possible.
-					echo -e "日本語雑魚\tBasic\t1\t$kanji<br>$meta\t$slot" >> $diclist
+					echo -e "日本語雑魚\tBasic\t1\t$kanji<br>$META\t$slot" >> $DICLIST
 
-				elif [ $mode == 'La' ]; then
-					slot=$(cat $tmp\
-						| sed -n "${bars[word-1]}, ${bars[word]} p"\
+				elif [ $MODE == 'La' ]; then
+					slot=$(cat $TEMP\
+						| sed -n "${BARS[word-1]}, ${BARS[word]} p"\
 						| grep -v '━'\
 						| sed 's/\s\{1,99\}/ /g'\
 						| sed 's/$/<br>/g'\
 						| tr '\n' ' ')
 
-					meta="$(echo $slot| sed 's/<br>/\n/g'| fgrep '[')"
-					echo -e "VOCABULAE\tBasic\t1\t$last<br>$meta\t$(echo $slot)" >> $diclist
+					META="$(echo $slot| sed 's/<br>/\n/g'| fgrep '[')"
+					echo -e "VOCABULAE\tBasic\t1\t$LAST<br>$META\t$(echo $slot)" >> $DICLIST
 
 				fi 
-				echo -e "$(echo -e "$last\n$meta"| sed 's/<br>/\n/g' ) "
-				rm -f $tmp
+				echo -e "$(echo -e "$LAST\n$META"| sed 's/<br>/\n/g' ) "
+				rm -f $TEMP
 			fi
 			;;
 
 		e)
 			echo -e "$bldylw >> English Mode.$txtrst"	
-			mode='En'
+			MODE='En'
 			;;
 
 		j)
 			echo -e "$bldylw >> 日本語モード$txtrst"	
-			mode='Ja'
+			MODE='Ja'
 			;;
 
 		l)
 			echo -e "$bldylw >> MODVSLATINVS$txtrst"	
-			mode='La'
+			MODE='La'
 			;;
 
 		pg)
 			echo -e "$bldylw >> Purge list.$txtrst"	
-			mv $diclist "$diclist"_bak
-			touch $diclist
+			mv $DICLIST "$DICLIST"_bak
+			touch $DICLIST
 			;;
 
 		d)
 			echo -e "$bldylw >> delete last entry.$txtrst"
-			tail -n1 $diclist| cut -d'<' -f1
-			sed -i '$d' $diclist
-			;&
+			tail -n1 $DICLIST| cut -d'<' -f1
+			sed -i '$d' $DICLIST
+			;&	# list all after deletion.
 
 		ls)
-			listtmp="$(cat $diclist\
+			listtmp="$(cat $DICLIST\
 				| cut -f1,4| cut -d'<' -f1\
 				| sed s/日本語雑魚/\\$bldwht/ \
 				| sed s/engmisc/\\$bldylw/ \
@@ -266,23 +278,23 @@ while read -e word; do
 			;;
 
 		*)
-			rm -f "$tmp"
-			last='' # clean last_word & metas.
-			meta=''
+			rm -f "$TEMP"
+			LAST='' # clean previous word & metas.
+			META=''
 
-			src=/tmp/.tridixsrc_$RANDOM
-			tmp=/tmp/.tridixtmp_$RANDOM
+			SOURCE=/tmp/.tridixsrc_$RANDOM
+			TEMP=/tmp/.tridixtmp_$RANDOM
 
-			if [ $mode == 'En' ]; then
+			if [ $MODE == 'En' ]; then
 				endic "$word"
-			elif [ $mode == 'Ja' ]; then
+			elif [ $MODE == 'Ja' ]; then
 				jadic "$word"
-			elif [ $mode == 'La' ]; then
+			elif [ $MODE == 'La' ]; then
 				ladic "$word"
 			fi
 
-			last="$word"
-			rm -f "$src"
+			LAST="$word"
+			rm -f "$SOURCE"
 			;;
 	esac
 
